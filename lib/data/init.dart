b836affe9921bc6ef168628c755bf9e25efa9963
Nyrzoh <anthony.verriest@hotmail.com>
2021-03-08 10:13:31 +0100

    Refactor DataInit

--- lib/data/init.dart
+++ lib/data/init.dart
@@ -1,9 +1,7 @@
 import 'package:projet4/data/repositories/game.dart';
 
 class DataInit {
-  static GameRepository _gameRepository = GameRepository();
-
   static Future<void> loadGameAssets() async {
-    await _gameRepository.loadRepository();
+    await GameRepository().loadRepository();
   }
 }
