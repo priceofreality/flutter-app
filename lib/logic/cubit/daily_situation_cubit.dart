d3aca2372b5024f3d4a16a9044c22cf1ef8ff05e
Nyrzoh <anthony.verriest@hotmail.com>
2021-03-08 13:47:36 +0100

    Init mapper choice-daily situation

--- lib/logic/cubit/daily_situation_cubit.dart
+++ lib/logic/cubit/daily_situation_cubit.dart
@@ -44,6 +44,7 @@ class DailySituationCubit extends Cubit<DailySituationState> {
 
   List<DailySituation>? _getNextDayDailySituations() {
     while (++_day < gameRepository.dailySituations.keys.length) {
+      //ggetmaxday
       if (gameRepository.dailySituations.containsKey(_day))
         return gameRepository.dailySituations[_day];
     }
